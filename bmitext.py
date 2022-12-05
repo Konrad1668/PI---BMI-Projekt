eef0ef158529fce9677de0d7ef0c42f2d9c80b3f
slo2cg022021 <81648787+slo2cg022021@users.noreply.github.com>
2022-12-05 14:40:34 +0100

    Add files via upload

---
+++ bmitext.py
@@ -0,0 +1,29 @@
+import csv
+
+pola = []
+wiersze = []
+
+with open(r'C:\Users\User\Desktop\danewejściowe.csv') as plikCSV:
+    czytnikCSV = csv.reader(plikCSV, delimiter=';')
+
+    pola = next(czytnikCSV)
+
+    for wiersz in czytnikCSV:
+     wiersze.append(wiersz)
+
+
+for wiersz in wiersze:
+    iloczyn = float(wiersz[1])
+    kwadrat = iloczyn**2
+
+
+for wiersz in wiersze:
+    iloraz = float(wiersz[0])
+    bmi = iloraz/kwadrat
+    print("BMI wynosi: ""%.2f " %bmi)
+
+    file = open("bmi.txt", "a+")
+    file.write(f"{bmi}")
+    file.close()
+
+
